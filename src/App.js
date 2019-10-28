6485152b17ebc6d40d6b3c71d959bd8532d3f199
Kenprogram <tadakenta@nakayukie-no-MacBook-Air.local>
2019-10-29 07:53:31 +0900

    Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
-class App extends React.Component{
-  render() {
-    const greeting = "Hi!";
-    const dom = <h1 className="foo">{greeting}</h1>;
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">
-          bar
-        </label>
-        <input type="text" onChange={()=> {console.log("I am clicked.")}} />
-      </React.Fragment>
-    );
-  }
+//class App extends React.Component{
+  //render() {
+    //const greeting = "Hi!";
+    //const dom = <h1 className="foo">{greeting}</h1>;
+    //return (
+      //<React.Fragment>
+        //<label htmlFor="bar">
+          //bar
+        //</label>
+        //<input type="text" onChange={()=> {console.log("I am clicked.")}} />
+      //</React.Fragment>
+    //);
+  //}
+//}
+
+const App = ()=> {
+  return ( 
+ <div>
+   <Cat />
+ </div>
+ )
+}
+
+const Cat = ()=>{ 
+  return (<div>Meao!</div>)
 }
 
 export default App;
