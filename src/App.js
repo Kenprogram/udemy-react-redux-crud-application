ad8b8fd570d9d0b72eaa6aab199e52ec96be2106
Kenprogram <tadakenta@nakayukie-no-MacBook-Air.local>
2019-10-30 23:48:51 +0900

    leran props of component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import React from 'react';
 
-//class App extends React.Component{
-  //render() {
-    //const greeting = "Hi!";
-    //const dom = <h1 className="foo">{greeting}</h1>;
-    //return (
-      //<React.Fragment>
-        //<label htmlFor="bar">
-          //bar
-        //</label>
-        //<input type="text" onChange={()=> {console.log("I am clicked.")}} />
-      //</React.Fragment>
-    //);
-  //}
-//}
 
 const App = ()=> {
+  const profiles = [
+      { name:"Taro", age:10},
+      { name:"Hanako", age:81},
+      { name:"no_name"},
+  ]
   return ( 
  <div>
-   <Cat />
+   {
+     profiles.map((profile, index)=>{
+        return <User name={profile.name} age={profile.age} key={index} />
+     })
+   }
  </div>
  )
 }
 
-const Cat = ()=>{ 
-  return (<div>Meao!</div>)
+const User = (props)=>{ 
+  return (<div>Hi!I am {props.name}.{props.age} years old! </div>)
+}
+User.defaultProps = {
+  age:1
 }
 
 export default App;
